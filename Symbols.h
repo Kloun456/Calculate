6dcb57290553e100540b9a06234401370d658302
Egor <78965361+Kloun456@users.noreply.github.com>
2021-07-06 15:07:12 +0300

    Update Symbols.h

--- Symbols.h
+++ Symbols.h
@@ -9,7 +9,7 @@ struct Symbols
 {
     const string s_numbers = "0123456789";
     const int numbers[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    vector <int> signs_index; // индексы математ. знаков
-    vector <int> brackets_index; // индексы скобок
-    map <int, double> results; // результаты линейных вычислений ханятся в виде: индекс-результат
+    vector <int> signs_index;
+    vector <int> brackets_index; 
+    map <int, double> results;
 }c;
