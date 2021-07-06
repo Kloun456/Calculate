2180f94d4953adc07c0145cc39b238b8bd623b2d
Egor <78965361+Kloun456@users.noreply.github.com>
2021-07-06 15:06:03 +0300

    Update Solution.h

--- Solution.h
+++ Solution.h
@@ -10,3 +10,4 @@ void Find_signs(string& s, int begin, int end);
 void Find_sings_and_brackets(string& s);
 void Operations(char s, int i1, double a, double b);
 void Check_on_Negative(string& s);
+void Trigonometry_func(string &s, char &func, int bracket1);
